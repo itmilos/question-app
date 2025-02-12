bc980eac36624606893bbae09ffbe0a9981859d7
itmilos <mikeruje@gmail.com>
2025-02-12 01:08:05 +0100

    Add hash routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import questionsData from './questions.js';
 import axios from 'axios';
 import Home from './Home';
